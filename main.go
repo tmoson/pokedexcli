2987c15f1bfd1fe4c74a5fbbe0c84b6cfbacdd71
tmoson <tyler.p.moson@gmail.com>
2026-06-12 11:35:21 -0400

    update repl to run help/exit commands

--- main.go
+++ main.go
@@ -1,17 +1,6 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		command := scanner.Text()
-		fmt.Printf("Your command was: %v\n", cleanInput(command)[0])
-	}
+	repl()
 }
--- repl.go
+++ repl.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"regexp"
 	"strings"
 )
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
 
 func cleanInput(text string) []string {
 	lowerText := strings.ToLower(strings.TrimSpace(text))
@@ -12,4 +21,35 @@ func cleanInput(text string) []string {
 	}
 	re := regexp.MustCompile(`\s+`)
 	return re.Split(lowerText, -1)
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func repl() {
+	commands := getCommands()
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		command := cleanInput(scanner.Text())[0]
+		action, ok := commands[command]
+		if !ok {
+			fmt.Printf("Unrecognized command: %v\n", command)
+			continue
+		}
+		action.callback()
+	}
 }
