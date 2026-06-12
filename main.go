e876b4633074a5b7e55d4dc35b9779cf3f92e7b1
tmoson <tyler.p.moson@gmail.com>
2026-06-12 10:47:48 -0400

    add initial repl logic

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
-
-func main(){
-	fmt.Println("Hello, World!")
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		command := scanner.Text()
+		fmt.Printf("Your command was: %v\n", cleanInput(command)[0])
+	}
 }
