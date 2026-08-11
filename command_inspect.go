2fb4d66fd9c2d9458464d25024c5d95a09e4a9f8
tmoson <tyler.p.moson@gmail.com>
2026-08-11 16:35:37 -0400

    remove old command functions

--- command_inspect.go
+++ command_inspect.go
@@ -1,32 +1,9 @@
 package main
 
 import (
-	"errors"
-	"fmt"
 	"charm.land/lipgloss/v2"
+	"fmt"
 )
-
-func commandInspect(conf *config, inputs ...string) error {
-	if len(inputs) == 0 {
-		return errors.New("Need a pokemon to inspect!")
-	}
-	pokemon := inputs[0]
-	info, caught := conf.pokedex[pokemon]
-	if !caught {
-		fmt.Printf("you have not caught %s!\n", pokemon)
-	} else {
-		formattedInfo := fmt.Sprintf("Name: %s\nHeight: %d\nWeight: %d\nStats:\n", info.Name, info.Height, info.Weight)
-		for _, stat := range info.Stats {
-			formattedInfo = fmt.Sprintf("%s -%s: %d\n", formattedInfo, stat.Stat.Name, stat.BaseStat)
-		}
-		formattedInfo = fmt.Sprintf("%sTypes:\n", formattedInfo)
-		for _, pokeType := range info.Types {
-			formattedInfo = fmt.Sprintf("%s  - %s\n", formattedInfo, pokeType.Type.Name)
-		}
-		fmt.Print(formattedInfo)
-	}
-	return nil
-}
 
 func commandInspectTea(conf *config, inputs ...string) string {
 	if len(inputs) == 0 {
